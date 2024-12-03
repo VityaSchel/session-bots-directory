924f0e915182517484d136a3fd2194dff97624de
VityaSchel <59040542+VityaSchel@users.noreply.github.com>
2024-12-03 19:33:07 +0400

    Make font in MigrationAlert look normal

--- app/widgets/migration-alert.tsx
+++ app/widgets/migration-alert.tsx
@@ -13,7 +13,7 @@ export function MigrationAlert() {
   }, [])
 
   return isMounted && isOldUrl && !closed && (
-    <div className='fixed top-0 left-0 z-[9999] bg-black/50 flex items-center justify-center w-full h-full'>
+    <div className='fixed top-0 left-0 z-[9999] bg-black/50 flex items-center justify-center w-full h-full font-sans'>
       <div className='bg-white p-4 rounded-sm text-black flex flex-col text-center items-center justify-center gap-2'>
         <h1 className='font-medium text-lg'>We&apos;re migrating</h1>
         <p>sessionbots.directory is now <a href='https://bots.session.community' className='underline font-medium'>bots.session.community</a></p>
